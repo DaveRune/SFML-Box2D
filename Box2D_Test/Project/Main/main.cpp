f2e9442460473abd9ca8e5e90b6f0dcacfcc933c
DaveRune <daverune.dk@gmail.com>
2013-11-07 02:43:51 +0000

    Graphical Enhancements
    New Artwork
    Reset Function Complete
    Window Scaling Bug Fixes

--- Box2D_Test/Project/Main/main.cpp
+++ Box2D_Test/Project/Main/main.cpp
@@ -12,24 +12,25 @@
 bool paused = true;
 vector2 cubeStart(100,100);
 
-int main(int argc, char** argv)
-{
-	sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
+b2Vec2 gravity(0.0f, 5.0f);
+b2World* world = new b2World(gravity);
+b2BodyDef bodyDef;
+b2Body* body = world->CreateBody(&bodyDef);
+b2PolygonShape dynamicBox;
+b2FixtureDef fixtureDef;
 
-	B2_NOT_USED(argc);
-	B2_NOT_USED(argv);
-
-	// Define the gravity vector.
+void CreateWorld() {
+  // Define the gravity vector.
 	b2Vec2 gravity(0.0f, 5.0f);
 
 	// Construct a world object, which will hold and simulate the rigid bodies.
-	b2World world(gravity);
+	b2World* world = new b2World(gravity);
 
 	// Define the dynamic body. We set its position and call the body factory.
 	b2BodyDef bodyDef;
 	bodyDef.type = b2_dynamicBody;
 	bodyDef.position.Set(cubeStart.x * SCALE ,cubeStart.y * SCALE);
-	b2Body* body = world.CreateBody(&bodyDef);
+	b2Body* body = world->CreateBody(&bodyDef);
   bodyDef.awake = false;
 	// Define another box shape for our dynamic body.
 	b2PolygonShape dynamicBox;
@@ -37,98 +38,181 @@ int main(int argc, char** argv)
 	// Define the dynamic body fixture.
 	b2FixtureDef fixtureDef;
 	fixtureDef.shape = &dynamicBox;
-  fixtureDef.restitution = 1.25f;
+  fixtureDef.restitution = 1.0f;
 	// Set the box density to be non-zero, so it will be dynamic.
 	fixtureDef.density = 1.0f;
 	// Override the default friction.
 	fixtureDef.friction = 0.3f;
 	// Add the shape to the body.
 	body->CreateFixture(&fixtureDef);
+}
+
+int main(int argc, char** argv)
+{
+	sf::RenderWindow window(sf::VideoMode(1000, 1000), "Physics Game!");
+
+  delete world;
+  CreateWorld();
+
+  #pragma region Background Graphics
+  // Load the background images, create a sprite and assign the image.
+  // Sky
+  sf::Texture backgroundTexture;
+  backgroundTexture.loadFromFile("assets/Sky.png");
+  backgroundTexture.setSmooth(true);
+  backgroundTexture.setRepeated(true);
+  sf::Sprite background;
+  background.setPosition(0,0);
+  background.setScale((float)window.getSize().x,1.0f);
+  background.setTexture(backgroundTexture);
+
+  // Sun
+  sf::Texture sunTexture;
+  sunTexture.loadFromFile("assets/Sun.png");
+  sunTexture.setSmooth(true);
+  sunTexture.setRepeated(true);
+  sf::Sprite sun;
+  sun.setPosition(window.getSize().x - 250 - window.getSize().x*0.2f,100);
+  sun.setScale(1.0f,1.0f);
+  sun.setTexture(sunTexture);
+
+  // Cloud
+  sf::Texture cloud1Texture;
+  cloud1Texture.loadFromFile("assets/Cloud1.png");
+  cloud1Texture.setSmooth(true);
+  cloud1Texture.setRepeated(false);
+  sf::Sprite cloud1;
+  cloud1.setPosition(window.getSize().x*0.15f,80);
+  cloud1.setScale(1.0f,1.0f);
+  cloud1.setTexture(cloud1Texture);
+
+  // Hills
+  sf::Texture hillsTexture;
+  hillsTexture.loadFromFile("assets/Hills.png");
+  hillsTexture.setSmooth(false);
+  hillsTexture.setRepeated(true);
+  sf::Sprite hills1;
+  hills1.setPosition(0,window.getSize().y - 184);
+  hills1.setScale(1.0f ,1.0f);
+  hills1.setTexture(hillsTexture);
+  sf::Sprite hills2;
+  hills2.setPosition(766,window.getSize().y - 184);
+  hills2.setScale(1.0f,1.0f);
+  hills2.setTexture(hillsTexture);
+  sf::Sprite hills3;
+  hills3.setPosition(766*2,window.getSize().y - 184);
+  hills3.setScale(1.0f,1.0f);
+  hills3.setTexture(hillsTexture);
+  #pragma endregion
 
 	// Prepare for simulation. Typically we use a time step of 1/60 of a
 	// second (60Hz) and 10 iterations. This provides a high quality simulation
 	// in most game scenarios.
-	float32 timeStep = 1.0f / 120.0f;
+	float32 timeStep = 1.0f / 60.0f;
 	int32 velocityIterations = 6;
-	int32 positionIterations = 2;  
+	int32 positionIterations = 2;
 
   std::vector<shape> lines;
   std::vector<b2Body> *b2lines;
 
-  bool doConstantLines = false;
-
+  #pragma region events
   while (window.isOpen()){
     sf::Event event;
     while (window.pollEvent(event))
     {
-        switch (event.type)
-        {
-        case sf::Event::Closed:
-            window.close();
-            break;
+      // If the window is closed, close the SFML window
+      switch (event.type) {
 
-        case sf::Event::MouseButtonPressed:
-            static bool pressed = false;
-            static vector2 start,end;
+      case sf::Event::Closed:
+          window.close();
+          break;
 
-            // When we click save the positions to a Vector2
-            if(event.mouseButton.button == sf::Mouse::Left) {
-              // If we are drawing lines constantly, dont perform click drawing
-              if(doConstantLines) break;
-              if(!pressed) {
-                start.x = event.mouseButton.x;
-                start.y = event.mouseButton.y;
-                pressed = true;
-              } else {
-                end.x = event.mouseButton.x;
-                end.y = event.mouseButton.y;
+      // Resize window : set new size
+      case sf::Event::Resized:
+          window.setView(sf::View(sf::FloatRect(0.0f, 0.0f, (float)event.size.width, (float)event.size.height)));
 
-                // Create the SFML visual box based on click locations
-                shape sf_shape = shape(start,end, 2);
-                lines.push_back(sf_shape);
+          // Rescale and Reposition the background elements
+          background.setScale((float)window.getSize().x,1.0f);
+          sun.setPosition(window.getSize().x - 250 - window.getSize().x*0.2f,100);
+          hills1.setPosition(0,window.getSize().y - 184);
+          hills2.setPosition(766,window.getSize().y - 184);
+          hills3.setPosition(766*2,window.getSize().y - 184);
+          break;
+
+      case sf::Event::MouseButtonPressed:
+          static bool pressed = false;
+          static vector2 start,end;
+
+          // When we click save the positions to a Vector2
+          if(event.mouseButton.button == sf::Mouse::Left) {
+            // If we are drawing lines constantly, dont perform click drawing
+            if(!pressed) {
+              start.x = event.mouseButton.x;
+              start.y = event.mouseButton.y;
+              pressed = true;
+            } else {
+              end.x = event.mouseButton.x;
+              end.y = event.mouseButton.y;
+
+              // Create the SFML visual box based on click locations
+              shape sf_shape = shape(start,end, 2);
+              lines.push_back(sf_shape);
         
-                vector2 center = center.GetCenter(start, end);
+              vector2 center = center.GetCenter(start, end);
 
-                // Create the Box2D physics box.
-                static b2FixtureDef myFixture;
-                static b2BodyDef myBody;
-                myBody.type = b2_staticBody;
-                myBody.position.Set(center.x, center.y);
-                b2Body* body = world.CreateBody(&myBody);
-                b2Vec2 vertices[4];
+              // Create the Box2D physics box.
+              static b2FixtureDef myFixture;
+              static b2BodyDef myBody;
+              myBody.type = b2_staticBody;
+              myBody.position.Set(center.x, center.y);
+              b2Body* body = world->CreateBody(&myBody);
+              b2Vec2 vertices[4];
 
-                // Caluclate the positions of the Box2D Verts
-                sf_shape.Box2DVertPos();
-                vertices[0].Set(sf_shape.a.x * SCALE -start.x*0.5f, sf_shape.a.y * SCALE -start.y*0.5f);
-                vertices[3].Set(sf_shape.b.x * SCALE -start.x*0.5f, sf_shape.b.y * SCALE -start.y*0.5f);
-                vertices[2].Set(sf_shape.c.x * SCALE -start.x*0.5f, sf_shape.c.y * SCALE -start.y*0.5f);
-                vertices[1].Set(sf_shape.d.x * SCALE -start.x*0.5f, sf_shape.d.y * SCALE -start.y*0.5f);
+              // Caluclate the positions of the Box2D Verts
+              sf_shape.Box2DVertPos();
+              vertices[0].Set(sf_shape.a.x * SCALE -start.x*0.5f, sf_shape.a.y * SCALE -start.y*0.5f);
+              vertices[3].Set(sf_shape.b.x * SCALE -start.x*0.5f, sf_shape.b.y * SCALE -start.y*0.5f);
+              vertices[2].Set(sf_shape.c.x * SCALE -start.x*0.5f, sf_shape.c.y * SCALE -start.y*0.5f);
+              vertices[1].Set(sf_shape.d.x * SCALE -start.x*0.5f, sf_shape.d.y * SCALE -start.y*0.5f);
 
-                b2PolygonShape polyShape;
-                polyShape.Set(vertices, 4);
+              b2PolygonShape polyShape;
+              polyShape.Set(vertices, 4);
                  
-                myFixture.shape = &polyShape;
-                myFixture.density = 1;
-                body->CreateFixture(&myFixture);
+              myFixture.shape = &polyShape;
+              myFixture.density = 1;
+              body->CreateFixture(&myFixture);
 
-                pressed = false;
-              }
+              pressed = false;
             }
+          }
         }
     }
+    #pragma endregion
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-      for(b2Body* b = world.GetBodyList(); b; b = b->GetNext()) {
+    #pragma region Reset 
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+      // Clear all SFML Lines
+      lines.clear();
+
+      // Reset the Box
+      for(b2Body* b = world->GetBodyList(); b; b = b->GetNext()) {
         if(b->GetType() == b2_dynamicBody) {
           b->SetTransform(b2Vec2(cubeStart.x * SCALE ,cubeStart.y * SCALE), 0 );
           b->SetLinearVelocity(b2Vec2(0.1f,0));
           b->SetAngularVelocity(0);
         }
       }
-    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::R)) {
-      lines.clear();
-    }
+      
+      // Pause the game
+      paused = true;
 
+      // Reset the World
+      delete world;
+      CreateWorld();
+    }
+    #pragma endregion 
+
+    #pragma region Pause
     static bool toggle = false;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
       if(!toggle) {
@@ -138,17 +222,27 @@ int main(int argc, char** argv)
     } else {
       toggle = false;
     }
+    #pragma endregion
 
-    if(!paused) world.Step(timeStep, velocityIterations, positionIterations);
+    if(!paused) world->Step(timeStep, velocityIterations, positionIterations);
 
     window.clear();
 
-    
+    #pragma region BackgroundArt
+    window.draw(background);
+    window.draw(sun);
+    window.draw(cloud1);
+    window.draw(hills1);
+    window.draw(hills2);
+    window.draw(hills3);
+    #pragma endregion
+
+    #pragma region DrawSFML
     size_t size = 0;
     sf::ConvexShape cShape;
     cShape.setFillColor(sf::Color::Red);
     //This loops through every single body in the game world
-    for(b2Body* b = world.GetBodyList(); b; b = b->GetNext()) {
+    for(b2Body* b = world->GetBodyList(); b; b = b->GetNext()) {
       //This loops through every fixture in the current body
       for(b2Fixture* f = b->GetFixtureList(); f; f = f->GetNext()) {
         //This checks to see if the type of the fixture is a polygon, if it is then draw the polygon
@@ -171,7 +265,9 @@ int main(int argc, char** argv)
         }
       }
     }
+    #pragma endregion
 
+    #pragma region DrawClickedLines
     // draw all elements in line vector
     for(int i = 0; i != lines.size(); ++i) {
       static sf::VertexArray quad(sf::Quads, 4);
@@ -179,9 +275,25 @@ int main(int argc, char** argv)
       quad[1].position = sf::Vector2f(lines[i].b.x, lines[i].b.y);
       quad[2].position = sf::Vector2f(lines[i].c.x, lines[i].c.y);
       quad[3].position = sf::Vector2f(lines[i].d.x, lines[i].d.y);
+      for(int j = 0; j != quad.getVertexCount(); ++j)
+        quad[j].color = sf::Color::Green;
       window.draw(quad);
-    }
 
+      // Create shadows under all the lines
+      static sf::VertexArray shadow(sf::Quads, 4);
+      shadow[0].position = sf::Vector2f(lines[i].a.x, lines[i].a.y);
+      shadow[1].position = sf::Vector2f(lines[i].b.x, lines[i].b.y);
+      shadow[2].position = sf::Vector2f(lines[i].c.x, lines[i].c.y+80);
+      shadow[3].position = sf::Vector2f(lines[i].d.x, lines[i].d.y+80);
+      shadow[0].color = sf::Color::Color(0,0,0,80);
+      shadow[1].color = sf::Color::Color(0,0,0,80);
+      shadow[2].color = sf::Color::Color(0,0,0,0);
+      shadow[3].color = sf::Color::Color(0,0,0,0);
+      window.draw(shadow);
+    }
+    #pragma endregion
+
+    #pragma region Play/Pause
     // Change the play and pause Icon on screen based on state
     if(!paused) {
       static sf::VertexArray play(sf::Triangles, 3);
@@ -217,6 +329,8 @@ int main(int argc, char** argv)
       window.draw(pause1);
       window.draw(pause2);
     }
+    #pragma endregion
+
 
   window.display();
   }
